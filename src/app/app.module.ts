f0edef656d8fc2e5a278cd6c4b90aed3ba0c68db
Dan Percic <danpercic86@gmail.com>
2021-01-17 18:40:21 +0200

    add ssr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule.withServerTransition({ appId: 'formidableServer' }), AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
 })
